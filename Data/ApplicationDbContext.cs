d6f48cb95f0c7af08827497fd7b3339126216053
Superhuman2609 <dangminhtuan000@gmail.com>
2022-09-18 21:23:02 +0700

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using testmvc.Models;
+using testmvc.Data;
+
+namespace testmvc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<test> tests { get; set; }
+    }
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace testmvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
